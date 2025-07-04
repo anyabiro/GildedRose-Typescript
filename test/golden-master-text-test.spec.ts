78c6463d8c1bf34a2026ed6eb78103c2097ef125
Anya Biro <Anya.Biro@softwire.com>
2025-07-04 14:39:06 +0300

    Added before hook.

--- test/golden-master-text-test.spec.ts
+++ test/golden-master-text-test.spec.ts
@@ -2,15 +2,24 @@ import { Item, GildedRose } from '../app/gilded-rose';
 import {expect} from "chai";
 
 // Add a master test here
+// describe('Special items', function () {
+//     let gildedRose: GildedRose;
+//     let items: Item[];
+
 describe('Random items after one day', function () {
-    const gildedRose = new GildedRose([
-        new Item('Papaya', 10, 30),
-        new Item('Peanuts', 25, 50),
-        new Item('Dragon fruit', 0, 10),
-        new Item('Purple potato', 0, 0),
-        new Item('Quality grapes', 5, 33)]
-    );
-    let items = gildedRose.updateQuality();
+    let gildedRose: GildedRose;
+    let items: Item[];
+
+    before(function () {
+        gildedRose = new GildedRose([
+            new Item('Papaya', 10, 30),
+            new Item('Peanuts', 25, 50),
+            new Item('Dragon fruit', 0, 10),
+            new Item('Purple potato', 0, 0),
+            new Item('Quality grapes', 5, 33)]
+        );
+        items = gildedRose.updateQuality();
+    });
 
     it('Papaya\'s Quality decreases by one after one day', function() {
         expect(items[0].quality).to.equal(29);
@@ -54,18 +63,23 @@ describe('Random items after one day', function () {
 });
 
 describe('Random items after five days', function () {
-    const gildedRose = new GildedRose([
-        new Item('Papaya', 10, 30),
-        new Item('Peanuts', 25, 50),
-        new Item('Dragon fruit', 0, 10),
-        new Item('Purple potato', 0, 0),
-        new Item('Quality grapes', 5, 33)]
-    );
-    let items = gildedRose.updateQuality();
-    items = gildedRose.updateQuality();
-    items = gildedRose.updateQuality();
-    items = gildedRose.updateQuality();
-    items = gildedRose.updateQuality();
+    let gildedRose: GildedRose;
+    let items: Item[];
+
+    before(function () {
+        gildedRose = new GildedRose([
+            new Item('Papaya', 10, 30),
+            new Item('Peanuts', 25, 50),
+            new Item('Dragon fruit', 0, 10),
+            new Item('Purple potato', 0, 0),
+            new Item('Quality grapes', 5, 33)]
+        );
+        items = gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+    });
 
     it('Papaya\'s Quality decreases by five after five days', function() {
         expect(items[0].quality).to.equal(25);
@@ -109,17 +123,22 @@ describe('Random items after five days', function () {
 });
 
 describe('Special items after one day', function () {
-    const gildedRose = new GildedRose([
-        new Item('Aged Brie', 10, 10),
-        new Item('Aged Brie', 2, 50),
-        new Item('Aged Brie', 66, 0),
-        new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-        new Item('Backstage passes to a TAFKAL80ETC concert', 15, 10),
-        new Item('Backstage passes to a TAFKAL80ETC concert', 9, 10),
-        new Item('Backstage passes to a TAFKAL80ETC concert', 3, 10),
-        new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)
-    ]);
-    const items = gildedRose.updateQuality();
+    let gildedRose: GildedRose;
+    let items: Item[];
+
+    before(function () {
+        gildedRose = new GildedRose([
+            new Item('Aged Brie', 10, 10),
+            new Item('Aged Brie', 2, 50),
+            new Item('Aged Brie', 66, 0),
+            new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 10),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 9, 10),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 3, 10),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)
+        ]);
+        items = gildedRose.updateQuality();
+    });
 
     it('Brie Quality increases by one after one day and SellIn decreases', function() {
         expect(items[0].quality).to.equal(11);
@@ -151,21 +170,26 @@ describe('Special items after one day', function () {
 });
 
 describe('Special items after 3 days', function () {
-    const gildedRose = new GildedRose([
-        new Item('Aged Brie', 10, 10),
-        new Item('Aged Brie', 2, 50),
-        new Item('Aged Brie', 66, 0),
-        new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-        new Item('Backstage passes to a TAFKAL80ETC concert', 15, 10),
-        new Item('Backstage passes to a TAFKAL80ETC concert', 9, 10),
-        new Item('Backstage passes to a TAFKAL80ETC concert', 3, 10),
-        new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)
-    ]);
-    let items = gildedRose.updateQuality();
-    items = gildedRose.updateQuality();
-    items = gildedRose.updateQuality();
+    let gildedRose: GildedRose;
+    let items: Item[];
 
-    it('Brie Quality increases by three after three day and SellIn decreases', function() {
+    before(function () {
+        gildedRose = new GildedRose([
+            new Item('Aged Brie', 10, 10),
+            new Item('Aged Brie', 2, 50),
+            new Item('Aged Brie', 66, 0),
+            new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 10),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 9, 10),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 3, 10),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)
+        ]);
+        items = gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+    });
+
+    it('Brie Quality increases by three after three day and SellIn decreases', function () {
         expect(items[0].quality).to.equal(13);
         expect(items[0].sellIn).to.equal(7);
         expect(items[1].quality).to.equal(50);
@@ -174,12 +198,12 @@ describe('Special items after 3 days', function () {
         expect(items[2].sellIn).to.equal(63);
     });
 
-    it('Sulfuras Quality and SellIn do not decrese', function() {
+    it('Sulfuras Quality and SellIn do not decrese', function () {
         expect(items[3].quality).to.equal(80);
         expect(items[3].sellIn).to.equal(0);
     });
 
-    it('Backstage passes increase in Quality as SellIn approaches', function() {
+    it('Backstage passes increase in Quality as SellIn approaches', function () {
         expect(items[4].quality).to.equal(13);
         expect(items[4].sellIn).to.equal(12);
         expect(items[5].quality).to.equal(16);
@@ -188,7 +212,7 @@ describe('Special items after 3 days', function () {
         expect(items[6].sellIn).to.equal(0);
     });
 
-    it('Backstage passes go to 0 Quality after sell by date has passed', function() {
+    it('Backstage passes go to 0 Quality after sell by date has passed', function () {
         expect(items[7].quality).to.equal(0);
         expect(items[7].sellIn).to.equal(-3);
     });
